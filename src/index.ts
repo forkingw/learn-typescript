d268596d1318a8dd3f87b6e4cdbbe94554395709
for <996500646@qq.com>
2020-10-02 00:34:52 +0800

    readonly

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,19 @@
-import { createDeck, printDeck } from "./funcs"
-
-const deck = createDeck()
-// interface User {
-//     name:string,
-//     age: number,
-//     sayHello(): void
-// }
-
-// type User = {
-//     name: string,
-//     age:  number,
-//     sayHello: () => void
-// }
-
-// let u: User = {
-//     name: "asdf",
-//     age: 33,
-//     sayHello() {}
-// }
-
-// type Condition = (n: number) => boolean
-// type Condition = {
-//     (n: number): boolean
-// }
-
-// interface Condition {
-//     (n:number): boolean
-// }
-// function sum(numbers: number[], callBack: Condition) {
-//     let s = 0;
-//     numbers.forEach(n => {
-//         if (callBack(n)) {
-//             s +=n;
-//         }
-//     })
-//     return s;
-// }
-// const result = sum([3,4,6,8], n => n % 2 !== 0);
-// console.log(result) 
-
-// interface A {
-//     T1: string
-// }
-
-// interface B extends A {
-//     T2: number
-// }
-
-// interface C extends A, B {
-//     T3: boolean
-// }
-
-type A = {
-    T1: string
+interface User {
+    readonly id:string,
+    name: string,
+    age: number,
+    readonly arr: readonly string[]
 }
 
-type B = {
-    T2: number
+let u:User = {
+    id: "23",
+    name: 'for',
+    age: 18,
+    arr: ['1', '4']
 }
 
-type C = {
-    T3: boolean
-} & A & B
+// u.id = "123"
 
-let u: C = {
-    T2: 33,
-    T3: true,
-    T1: "33"
-}
+// let arr: readonly number[] = [3,3,4,4]
+
+// let arr: ReadonlyArray<number> = [3, 4, 6]
