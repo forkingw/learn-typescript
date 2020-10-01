85eb35e82a8e0d40a33399fa27d4c3fbf3c44d0d
for <996500646@qq.com>
2020-10-02 01:19:46 +0800

    type-compatibal

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
-interface User {
-    readonly id:string,
-    name: string,
-    age: number,
-    readonly arr: readonly string[]
+interface Duck {
+    sound: "嘎嘎",
+    swin(): void
+}
+const person = {
+    name: "装成鸭子的人",
+    age: 34,
+    sound: "嘎嘎" as "嘎嘎",
+    swin() {
+        console.log(this.name + " is swiming, and sound" + this.sound)
+    }
 }
 
-let u:User = {
-    id: "23",
-    name: 'for',
-    age: 18,
-    arr: ['1', '4']
-}
-
-// u.id = "123"
-
-// let arr: readonly number[] = [3,3,4,4]
-
-// let arr: ReadonlyArray<number> = [3, 4, 6]
+let duck: Duck = person
