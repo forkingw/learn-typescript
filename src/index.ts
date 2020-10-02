a80e1933fa57a004d098afda76e95b68bb3c0f60
for <996500646@qq.com>
2020-10-02 17:42:01 +0800

    func-generics

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-import { Deck } from "./deck"
+function take<T>(arr: T[], n:number) {
+    if (n >= arr.length) {
+        return arr;
+    }
+    const newArr: T[] = [];
+    for (let i = 0; i < n; i++) {
+        newArr.push(arr[i]);
+    }
+    return newArr;
+}
 
-const deck = new Deck()
-deck.shuffle()
-console.log("======== 洗牌之后 ========")
-deck.print()
-const result = deck.publish()
-console.log("======== 发牌之后 ========")
-
-console.log("======== player1 ========")
-result.player1.print()
-
-console.log("======== player2 ========")
-result.player2.print()
-
-console.log("======== player3 ========")
-result.player3.print()
-
-console.log("======== left ========")
-result.left.print()
+// const newArr = take<number>([1, 2, 4], 2)
+const newArr = take<object>([{},{},{}], 3)
+console.log(newArr);
