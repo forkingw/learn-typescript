9112b2776c7f8a6fdb208b0fa055458e4ac296c0
for <996500646@qq.com>
2020-10-04 12:13:36 +0800

    abstract-class

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,63 @@
-export class Tank {
-    x:number = 0
-    y:number = 0
+abstract class Chess {
+    x: number = 0
+    y: number = 0
 
-    protected name: string="tank"
+    abstract readonly name: string;
 
-    shoot() {
-        console.log("Take shoot")
+    move(targetX:number, targetY:number):boolean {
+        console.log("边界检查")
+        console.log("是否有己方棋子")
+        if (this.rule(targetX, targetY)) {
+            this.rule(targetX, targetY)
+            return true;
+        }
+        return false;
+
+    }
+    protected abstract rule (targetX:number, targetY:number):boolean;
+}
+
+class Horse extends Chess {
+    protected rule(targetX: number, targetY: number): boolean {
+        throw new Error("Method not implemented.");
+    }
+    move(targetX: number, targetY: number): boolean {
+        throw new Error("Method not implemented.");
+    }
+    readonly name: string = "马";
+    
+}
+
+class Solider extends Chess {
+    protected rule(targetX: number, targetY: number): boolean {
+        throw new Error("Method not implemented.");
+    }
+    move(targetX: number, targetY: number): boolean {
+        throw new Error("Method not implemented.");
+    }
+    readonly name: string;
+
+    constructor() {
+        super();
+        this.name = "炮";
+    }
+
+}
+
+class Cannon extends Chess {
+    protected rule(targetX: number, targetY: number): boolean {
+        throw new Error("Method not implemented.");
+    }
+    move(targetX: number, targetY: number): boolean {
+        throw new Error("Method not implemented.");
+    }
+    get name() {
+        return "兵";
     }
 }
 
-export class PlayerTank extends Tank {
-    x:number = 20
-    y:number = 20
+const horse = new Horse()
+const solider = new Solider()
+const cannon = new Cannon()
 
-    sayHello() {
-        console.log(this.name)
-    }
-
-    shoot() {
-        console.log("PlayerTank shoot")
-    }
-}
-
-export class EnemyTank extends Tank {
-    shoot() {
-        console.log("EmenyTank shoot")
-    }
-}
-
-const p = new PlayerTank()
-console.log(p.x, p.y)
+console.log(horse.name, solider.name, cannon.name);
