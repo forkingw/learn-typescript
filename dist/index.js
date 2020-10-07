809e82965dc7a9f151dee5493e4b25ca4ed89909
for <996500646@qq.com>
2020-10-07 11:53:22 +0800

    declaration

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,4 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-function test(target) {
-    console.log(target);
+function sum(a, b) {
+    return a + b;
 }
-let A = class A {
-};
-A = __decorate([
-    test
-], A);
+console.log(1, 2);
---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+// declare var console: {
+//     log(message?: any): void;
+// };
+
+// declare namespace console {
+//     function log(message?: any): void
+//     function error(message?: any): void
+// }
